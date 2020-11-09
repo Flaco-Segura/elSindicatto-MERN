36e8c661fc68e24a9d8d5572b08ff6b838891f5a
Scarnouse <manquesada@gmail.com>
2020-11-09 06:38:46 +0100

    refactoring rating

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -2,26 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
+    return (
+        <div className='rating'>
+            {stars(value, color)}
+            <span>{text && text}</span>
+        </div>
+    )
+}
+
+const stars = (value, color) => {
     let stars = [];
 
     [1,2,3,4,5].forEach(position => {
         stars.push(
             <span key={position}>
                 <i  style={{color}}
-                    className={setIcon(value, position)}></i>
+                    className={icon(value, position)}></i>
             </span>
         )
     });
 
-    return (
-        <div className='rating'>
-            {stars}
-            <span>{text && text}</span>
-        </div>
-    )
+    return stars
 }
 
-const setIcon = (value, position) => {
+const icon = (value, position) => {
     if (value < position && value > position - 1) {
         return 'fas fa-star-half-alt';
     } else if (value < position) {

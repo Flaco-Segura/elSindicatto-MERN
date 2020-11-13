6681ada3ba4c4ddd8f609d8ff8b48410a2676502
Scarnouse <manquesada@gmail.com>
2020-11-13 08:07:52 +0100

    connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import discs from './data/discs.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 

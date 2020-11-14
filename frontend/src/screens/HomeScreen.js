6ee2633a4ad1b928cca31295e53ae891f2cf8568
Scarnouse <manquesada@gmail.com>
2020-11-14 08:35:48 +0100

    bringing redux state into homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Disc from '../components/Disc';
-import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import Disc from '../components/Disc'
+import { listDisc } from '../actions/discActions'
 
 const HomeScreen = () => {
-    const [discs, setDiscs] = useState([]);
+    const dispatch = useDispatch()
+
+    const discList = useSelector( state => state.discList )
+    const { loading, error, discs } = discList
 
     useEffect(() => {
-        const fetchDiscs = async () => {
-            const { data } = await axios.get('/api/discs')
-
-            setDiscs(data)
-        }
-
-        fetchDiscs()
-    }, [])
+        dispatch(listDisc())
+    }, [dispatch])
 
     return (
         <>
             <h1>Latest Discs</h1>
-            <Row>
-                {discs.map(disc => (
-                    <Col key={disc._id} sm={12} md={6} lg={4} xl={3}>
-                        <Disc disc={disc} />
-                    </Col>
-                ))}
-            </Row>  
+
+            { loading 
+                ? <h2>Loading...</h2>
+                : error
+                    ? <h3>{error}</h3>
+                    : <Row>
+                        {discs.map(disc => (
+                            <Col key={disc._id} sm={12} md={6} lg={4} xl={3}>
+                                <Disc disc={disc} />
+                            </Col>
+                        ))}
+                      </Row>
+            }  
         </>
     )
 }

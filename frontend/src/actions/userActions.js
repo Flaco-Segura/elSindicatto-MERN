6f94084cbf6b06954ee9a478a52ff8b359bc970c
Scarnouse <manquesada@gmail.com>
2020-11-20 11:26:24 +0100

    fixing userActions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,7 +4,7 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS } from "../constants/userConstants"
 
-export const login = (email, password) => async(dispatch) {
+export const login = (email, password) => async(dispatch) => {
     try {
         dispatch({
             type: USER_LOGIN_REQUEST

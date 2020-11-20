293777be739e36a25f251ec4010e24e2c229a612
Scarnouse <manquesada@gmail.com>
2020-11-20 09:54:41 +0100

    custom authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,7 @@ import generateToken from '../utils/generateToken.js'
 const authUser = asyncHandler(async(req, res) => {
     const { email, password } = req.body
 
-    const user = await User.findOne({ email})
+    const user = await User.findOne({ email })
 
     if(user && (await user.matchPassword(password))) {
         res.json({
@@ -24,4 +24,23 @@ const authUser = asyncHandler(async(req, res) => {
     }
 })
 
-export { authUser }
+// @desc    Get user profile
+// @route   POST /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error('Not found')
+    }
+})
+
+export { authUser, getUserProfile }

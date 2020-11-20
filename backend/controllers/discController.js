d5ffc881d9df54294a4a885ad4d089a880e1062f
Scarnouse <manquesada@gmail.com>
2020-11-20 08:47:39 +0100

    clean up by using controllers

---
+++ backend/controllers/discController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler'
+import Disc from '../models/discModel.js'
+
+// @desc    Fetch all discs
+// @route   GET /api/discs
+// @access  Public
+const getDiscs = asyncHandler(async(req, res) => {
+    const discs = await Disc.find({})
+
+    res.json(discs)
+})
+
+// @desc    Fetch single disc
+// @route   GET /api/discs/:id
+// @access  Public
+const getDiscById = asyncHandler(async(req, res) => {
+    const disc = await Disc.findById(req.params.id)
+
+    if (disc) {
+        res.json(disc)
+    } else {
+        res.status(404)
+        throw new Error('Disc not found')
+    }
+})
+
+export { getDiscs, getDiscById }
--- backend/routes/discRoutes.js
+++ backend/routes/discRoutes.js
@@ -1,29 +1,8 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Disc from '../models/discModel.js'
+import { getDiscs, getDiscById } from '../controllers/discController.js'
 
-// @desc    Fetch all discs
-// @route   GET /api/discs
-// @access  Public
-router.get('/', asyncHandler(async (req, res) => {
-    const discs = await Disc.find({})
-
-    res.json(discs)
-}))
-
-// @desc    Fetch single disc
-// @route   GET /api/discs/:id
-// @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    const disc = await Disc.findById(req.params.id)
-
-    if (disc) {
-        res.json(disc)
-    } else {
-        res.status(404)
-        throw new Error('Disc not found')
-    }
-}))
+router.route('/').get(getDiscs)
+router.route('/:id').get(getDiscById)
 
 export default router

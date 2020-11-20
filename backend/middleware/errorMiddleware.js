44d49f70a6445279399c33a6bc0512555a7e0a94
Scarnouse <manquesada@gmail.com>
2020-11-20 12:18:07 +0100

    user login screen & functionality

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,7 +8,6 @@ const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
     res.status(statusCode)
     res.json({
-        status: res.statusCode,
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
     })

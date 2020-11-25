e8496ff17de50a4319f1cfff73b173532fbe648d
Scarnouse <manquesada@gmail.com>
2020-11-25 09:04:17 +0100

    refactoring checkoutsteps

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -2,43 +2,38 @@ import React from 'react'
 import { Nav } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
-const CheckoutSteps = ({step1, step2, step3, step4}) => {    
+const CheckoutSteps = ({step1, step2, step3, step4}) => {
     return (
         <Nav className='justify-content-center mb-4'>
-            <Nav.Item>
-                {step1 
-                    ? <LinkContainer to='/login'>
-                        <Nav.Link>Sign In</Nav.Link>
-                    </LinkContainer>
-                    : <Nav.Link disabled>Sign In</Nav.Link>
-                }
-            </Nav.Item>
-            <Nav.Item>
-                {step2
-                    ? <LinkContainer to='/shipping'>
-                        <Nav.Link>Shipping</Nav.Link>
-                    </LinkContainer>
-                    : <Nav.Link disabled>Shipping</Nav.Link>
-                }
-            </Nav.Item>
-            <Nav.Item>
-                {step3
-                    ? <LinkContainer to='/payment'>
-                        <Nav.Link>Payment</Nav.Link>
-                    </LinkContainer>
-                    : <Nav.Link disabled>Payment</Nav.Link>
-                }
-            </Nav.Item>
-            <Nav.Item>
-                {step4 
-                    ? <LinkContainer to='/placeorder'>
-                        <Nav.Link>Place Order</Nav.Link>
-                    </LinkContainer>
-                    : <Nav.Link disabled>Place Order</Nav.Link>
-                }
-            </Nav.Item>
+            { navItems({step1, step2, step3, step4}) }
         </Nav>
     )
+}
+
+const navItems = steps => {
+    const item = {
+        step1: ['/login', 'Sign In'],
+        step2: ['/shipping', 'Shipping'],
+        step3: ['/payment', 'Payment'],
+        step4: ['/placeorder', 'Place Order']
+    }
+
+    const navItems = []
+
+    for (const step in steps) {
+        navItems.push(
+            <Nav.Item key={step}>
+                { steps[step]
+                    ? <LinkContainer to={item[step][0]}>
+                        <Nav.Link>{ item[step][1] }</Nav.Link>
+                      </LinkContainer>
+                    : <Nav.Link disabled>{ item[step][1] }</Nav.Link>
+                }
+            </Nav.Item>
+        )
+    }
+
+    return navItems
 }
 
 export default CheckoutSteps

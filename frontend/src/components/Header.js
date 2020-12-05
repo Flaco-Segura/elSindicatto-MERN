c49581f320e6a67c269c0c46c6e2566780cc9871
Scarnouse <manquesada@gmail.com>
2020-12-05 09:02:22 +0100

    admin user list

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -42,7 +42,19 @@ const Header = () => {
                                 <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
                             </LinkContainer> 
                         }
-                                         
+                        { userInfo && userInfo.isAdmin && (
+                            <NavDropdown title='Admin' id='adminmenu'>
+                                <LinkContainer to='/admin/userlist'>
+                                    <NavDropdown.Item>Users</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to='/admin/disclist'>
+                                    <NavDropdown.Item>Discs</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to='/admin/orderlist'>
+                                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                                </LinkContainer>
+                            </NavDropdown>
+                        )}              
                     </Nav>
                 </Navbar.Collapse>
             </Container>        

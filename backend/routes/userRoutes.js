f7d41bbf938eb4bc3640bdf39f814dd2777aea56
Scarnouse <manquesada@gmail.com>
2020-12-05 08:21:56 +0100

    admin middleware && get users endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,13 @@ import {
     authUser,
     getUserProfile,
     registerUser,
-    updateUserProfile 
+    updateUserProfile,
+    getUsers
 } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 
 export default router

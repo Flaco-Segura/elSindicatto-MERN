526b6a95608d8ba24f5d04820baa3291bf63695b
Scarnouse <manquesada@gmail.com>
2020-12-06 07:33:53 +0100

    admin user delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,14 @@ import {
     getUserProfile,
     registerUser,
     updateUserProfile,
-    getUsers
+    getUsers,
+    deleteUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.route('/:id').delete(protect, admin, deleteUser)
 
 export default router

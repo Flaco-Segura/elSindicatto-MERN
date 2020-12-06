473434f963a3a159d4f77e6cea65d8d3f5b06691
Scarnouse <manquesada@gmail.com>
2020-12-06 07:08:57 +0100

    admin screen access security

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,7 +16,8 @@ import {
     USER_DETAILS_RESET,
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
-    USER_LIST_FAIL
+    USER_LIST_FAIL,
+    USER_LIST_RESET
 } from "../constants/userConstants"
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 import { CART_RESET } from '../constants/cartConstants'
@@ -57,6 +58,7 @@ export const logout = () => (dispatch) => {
     dispatch({ type: USER_LOGOUT })
     dispatch({ type: USER_DETAILS_RESET })
     dispatch({ type: ORDER_LIST_MY_RESET })
+    dispatch({ type: USER_LIST_RESET })
     dispatch({ type: CART_RESET })
 }
 
